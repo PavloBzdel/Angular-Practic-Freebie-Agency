f389135c3b202004dc504ad4a7c6662e2574500a
Pavlo Bzdel <pavlobzdel@gmail.com>
2023-01-05 16:58:49 +0200

    fixed images deployed

--- src/app/core/card-list.ts
+++ src/app/core/card-list.ts
@@ -8,20 +8,20 @@ export interface cardListing {
 export const card: cardListing[] = [
   {
     title: 'Branding',
-    svg: '/assets/icon/Services/arrow.svg',
-    icon: '/assets/icon/Services/services-branding.svg',
+    svg: 'assets/icon/Services/arrow.svg',
+    icon: 'assets/icon/Services/services-branding.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
   },
   {
     title: 'DESIGN',
-    svg: '/assets/icon/Services/arrow.svg',
-    icon: '/assets/icon/Services/services-design.svg',
+    svg: 'assets/icon/Services/arrow.svg',
+    icon: 'assets/icon/Services/services-design.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
   },
   {
     title: 'DEVELOPMENT',
-    svg: '/assets/icon/Services/arrow.svg',
-    icon: '/assets/icon/Services/services-development.svg',
+    svg: 'assets/icon/Services/arrow.svg',
+    icon: 'assets/icon/Services/services-development.svg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
   },
 ];
--- src/app/core/team-list.ts
+++ src/app/core/team-list.ts
@@ -7,19 +7,19 @@ export interface teamListing{
 
 export const teamCard: teamListing[]= [
   {
-    img: '/assets/images/Team/Team-Person1.png',
+    img: 'assets/images/Team/Team-Person1.png',
     name: 'TOM JONES',
     rank: 'Creative Director',
     description: 'Create with simplicity in mind!',
   },
   {
-    img: '/assets/images/Team/Team-Person2.png',
+    img: 'assets/images/Team/Team-Person2.png',
     name: 'MICHAEL FRAUP',
     rank: 'Creative Director',
     description: 'Create with simplicity in mind!',
   },
   {
-    img: '/assets/images/Team/Team-Person3.png',
+    img: 'assets/images/Team/Team-Person3.png',
     name: 'TIM CAHILL',
     rank: 'Creative Director',
     description: 'Create with simplicity in mind!',
