7280a92200faacf748446be1385c460fd12d8130
Pavlo Bzdel <pavlobzdel@gmail.com>
2023-01-05 16:46:05 +0200

    fixed 3

--- src/app/core/works-list.ts
+++ src/app/core/works-list.ts
@@ -7,13 +7,13 @@ export interface workListing{
 
 export const worksCardLeft: workListing[] = [
   {
-    img: '/assets/images/Works/Works-Cat.png',
+    img: 'assets/images/Works/Works-Cat.png',
     title: 'A KITTEN’S LIFE',
     subtitle: 'Photography',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   {
-    img: '/assets/images/Works/Works-notebook.png',
+    img: 'assets/images/Works/Works-notebook.png',
     title: 'APPLE’s LOST MAGIC',
     subtitle: 'Photography',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
@@ -22,13 +22,13 @@ export const worksCardLeft: workListing[] = [
 
 export const worksCardRight: workListing[]= [
   {
-    img: '/assets/images/Works/Works-photography.png',
+    img: 'assets/images/Works/Works-photography.png',
     title: '100 YEARS PHOTOGRAPHY',
     subtitle: 'Photography',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   {
-    img: '/assets/images/Works/Works-Mac.png',
+    img: 'assets/images/Works/Works-Mac.png',
     title: 'GROOVEMADE PRODUCTS FOR MAC',
     subtitle: 'Photography',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
